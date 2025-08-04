305a01e5c81ffbaf82c91c23d89b497fca87d3d7
Kenntomi <marci.kenntomi@gmail.com>
2025-08-04 18:20:20 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/mi-aplicacion-react/',  
-})
+  base: '/mi-aplicacion-react/',
+  plugins: [react()]
+});
